bfe14bfdc34189b22feeec146862b1bf133001fa
Matheus Passos <matheushtip@gmail.com>
2021-07-11 14:22:49 -0300

    :white_check_mark: Create the auth route to pass the auth test

--- users/urls.py
+++ users/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from users import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('auth', views.auth_user, name='auth'),
+    path('dashboard', views.dashboard, name='dashboard')
 ]
--- users/views.py
+++ users/views.py
@@ -1,5 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
 
 
 def index(request):
+    return render(request, 'home.html')
+
+
+def auth_user(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+        user = authenticate(username=email, password=password)
+        if not user:
+            return redirect('index')
+
+        login(request, user)
+        return redirect('dashboard')
+
+
+def dashboard(request):
     return render(request, 'home.html')
