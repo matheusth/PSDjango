25808f01bc446f584241dccabbb60dc554aa71e7
Matheus Passos <matheushtip@gmail.com>
2021-07-11 14:33:32 -0300

    :memo: add doc strings to the test methods

--- users/tests/test_custom_user.py
+++ users/tests/test_custom_user.py
@@ -8,14 +8,23 @@ class TestCustomUser(TestCase):
         self.fullname = 'Elixir Golang'
 
     def test_create_user_should_throw_error_if_no_email_is_provided(self):
+        """
+        Tests if the create_user method throws an error, in case the email is not provided.
+        """
         with self.assertRaises(ValueError):
             CustomUser.objects.create_user(fullname=self.fullname, email='', password='teste564#')
 
     def test_create_user_should_throw_error_if_no_password_is_provided(self):
+        """
+        Tests if the create_user method throws an error in case no password is provided
+        """
         with self.assertRaises(ValueError):
             CustomUser.objects.create_user(fullname=self.fullname, email='teste@teste.com', password='')
 
     def test_create_user_should_create_a_user_if_provided_all_information(self):
+        """
+        Tests if the create user method works in case the fullname, the email and the password are provided
+        """
         user = CustomUser.objects.create_user(fullname=self.fullname, email='teste@teste.com', password='teste564#')
 
         self.assertEqual(user.fullname, self.fullname.upper())
--- users/tests/test_views.py
+++ users/tests/test_views.py
@@ -4,11 +4,17 @@ from users.models import CustomUser
 
 class TestUserViews(TestCase):
     def test_unloged_user_should_see_the_home_page(self):
+        """
+            Tests if it is possible to view the homepage
+        """
         response = self.client.get('/')
 
         self.assertTemplateUsed(response, 'home.html')
 
     def test_registered_user_should_be_able_to_authenticate(self):
+        """
+        Tests if a registred user is able to authenticate with success on the auth view
+        """
         email = 'test_auth@teste.com'
         password = 'teste1234#'
         CustomUser.objects.create_user(fullname='Python 3', email=email, password=password)
